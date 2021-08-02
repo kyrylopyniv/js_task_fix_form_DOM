22518482d24d4ff75c224a5618a578d4b0912699
Kyrylo Pyniv <pynivkm@gmail.com>
2021-08-02 22:14:29 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
+const inputs = [...document.querySelectorAll('input')];
+
+for (const input of inputs) {
+  input.placeholder = firstLetterCaps(input.name);
+
+  input.insertAdjacentHTML('beforebegin',
+    `<label class='field-label' for=${input.id}>${input.name}</label>`
+  );
+};
+
+function firstLetterCaps(text) {
+  const result = text.toLowerCase();
+
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
 // write code here
